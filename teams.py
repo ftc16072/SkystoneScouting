9f3dc630cf3e963f119a6d959b10d0293b2073bd
philip smith <philip@randomsmiths.com>
2020-02-27 17:15:43 -0500

    Added Pit Num to team page

--- teams.py
+++ teams.py
@@ -2,10 +2,11 @@ import sqlite3
 import os
 
 class Team():
-    def __init__(self, teamNumber, teamName, teamID):
+    def __init__(self, teamNumber, teamName, teamID, pitNum):
         self.teamNumber = teamNumber
         self.teamName = teamName
         self.teamId = teamID
+        self.pitNum = pitNum
     
     def __str__(self):
         return f"{self.teamId}: {self.teamNumber} -- {self.teamName}"
@@ -19,29 +20,30 @@ class Teams():
         CREATE TABLE Teams(
             id INTEGER PRIMARY KEY,
             number INTEGER NOT NULL,
-            name STRING NOT NULL
+            name STRING NOT NULL,
+            pitNum INTEGER
         )""")
 
-    def addTeam(self, teamNumber, teamName, dbConnection):
-        dbConnection.execute("INSERT INTO Teams (number, name) VALUES (?,?)", (teamNumber, teamName))
+    def addTeam(self, teamNumber, teamName, pitNum, dbConnection):
+        dbConnection.execute("INSERT INTO Teams (number, name, pitNum) VALUES (?,?,?)", (teamNumber, teamName, pitNum))
     
     def getTeamList(self, dbConnection):
         teamList = []
-        for row in dbConnection.execute("SELECT number, name, id FROM Teams"):
-            teamList.append(Team(row[0], row[1], row[2]))
+        for row in dbConnection.execute("SELECT number, name, id, pitNum FROM Teams"):
+            teamList.append(Team(row[0], row[1], row[2], row[3]))
         return teamList
 
     def getTeamfromID(self, dbConnection, teamId):
-        for row in dbConnection.execute("SELECT number, name, id FROM Teams WHERE id=?", (teamId,)):
-            return Team(row[0], row[1],row[2])
+        for row in dbConnection.execute("SELECT number, name, id, pitNum FROM Teams WHERE id=?", (teamId,)):
+            return Team(row[0], row[1],row[2], row[3])
     
     def getTeamfromNum(self, dbConnection, teamNum):
-        for row in dbConnection.execute("SELECT number, name, id FROM Teams WHERE number=?", (teamNum,)):
-            return Team(row[0], row[1],row[2])
+        for row in dbConnection.execute("SELECT number, name, id, pitNum FROM Teams WHERE number=?", (teamNum,)):
+            return Team(row[0], row[1],row[2], row[3])
     
     def setupTeams(self, teamDict, dbConnection):
-        for number, name in teamDict.items():
-            self.addTeam(number, name, dbConnection)
+        for number, infoList in teamDict.items():
+            self.addTeam(number, infoList[0], infoList[1], dbConnection)
 
 
 if __name__ == "__main__":
